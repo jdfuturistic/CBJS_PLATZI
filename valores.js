4b1a19d6ffd9cbaef0d3fa5d3740b85ca9028234
Joseph David <josephdavid200320@gmail.com>
2023-03-30 07:48:08 -0500

    Valores primitivos y no primitivos para JS

---
+++ valores.js
@@ -0,0 +1,25 @@
+// Valores primitivos
+
+// valor tipo número
+40
+
+// valor tipo String
+"Diego de Granda"
+
+// valores tipo booleanos
+true
+false
+
+// valores vacíos o de error
+null
+undefined
+
+
+
+//Valores no primitivos o de tipo objeto
+
+// Objeto tipo Array
+[1,2,3]
+
+// valor tipo objeto
+{nombre: "Diego"}
